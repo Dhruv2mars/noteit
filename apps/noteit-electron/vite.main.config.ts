52a33dc3e418d411211295d1465ac21257dfcb84
me-DhruvSharma <dhruv.sharma10102005@gmail.com>
2025-06-24 21:50:08 +0530

    fix(build): Configure Vite to correctly build main and preload scripts

--- apps/noteit-electron/vite.main.config.ts
+++ apps/noteit-electron/vite.main.config.ts
@@ -1,4 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, '.vite/build'),
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['cjs'],
+      fileName: 'main',
+    },
+    rollupOptions: {
+      // Prevent bundling of external dependencies
+      external: ['electron', 'electron-updater', 'electron-log/main'],
+    },
+    // Prevent wiping other build artifacts
+    emptyOutDir: false,
+  },
+});
--- apps/noteit-electron/vite.preload.config.ts
+++ apps/noteit-electron/vite.preload.config.ts
@@ -1,4 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, '.vite/build'),
+    lib: {
+      entry: resolve(__dirname, 'src/preload.ts'),
+      formats: ['cjs'],
+      fileName: 'preload',
+    },
+    rollupOptions: {
+      // Prevent bundling of external dependencies
+      external: ['electron'],
+    },
+    // Prevent wiping other build artifacts
+    emptyOutDir: false,
+  },
+});
